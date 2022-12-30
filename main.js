0014ba68ab06a852237df7db63575fceba11407f
Benwhite7 <melendezcastilloemer@gmail.com>
2022-12-30 11:38:27 -0500

    Mejora

--- main.js
+++ main.js
@@ -3,114 +3,119 @@ let desktopMenu = document.querySelector('.desktop-menu');
 let menuHamIcon = document.querySelector('.menu');
 let mobileMenu = document.querySelector('.mobile-menu');
 let menuCarrito = document.querySelector('.navbar-shopping-cart');
-let shoppingCartContainer = document.querySelector ('#shoppingCartContainer');
-let cardsContainer=document.querySelector('.cards-container');
-let productDetailContainer = document.querySelector('#productDetail');
-let productDetailCloseBotom = document.querySelector('.product-detail-close');
+let shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+let cardsContainer = document.querySelector('.cards-container');
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
-menuHamIcon.addEventListener('click',toggleMobileMenu);
-menuCarrito.addEventListener('click',toggleCarritoAside);
-productDetailCloseBotom.addEventListener('click',closeProductDetailAside)
+let productDetailContainer = document.querySelector('#productDetailone');
+let productDetailContainertwo = document.querySelector('#productDetailtwo');
+let productDetailContainerthree = document.querySelector('#productDetailthree');
+let productDetailContainerfour = document.querySelector('#productDetailfour');
+let productDetailCloseBotom = document.querySelector('.product-detail-close');
+let productDetailCloseBotomtwo = document.querySelector('.product-detail-close2');
+let productDetailCloseBotomthree = document.querySelector('.product-detail-close3');
+let productDetailCloseBotomFour = document.querySelector('.product-detail-close4');
+
+let ShoesOne = document.querySelector(".shoes-one");
+let ShoesTwo = document.querySelector(".shoes-two");
+let ShoesThree = document.querySelector(".shoes-three");
+let ShoesFour = document.querySelector(".shoes-four");
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarritoAside);
+ShoesOne.addEventListener("click", openProductDetailAsideone);
+ShoesTwo.addEventListener("click", openProductDetailAsidetwo);
+ShoesThree.addEventListener("click", openProductDetailAsidethree);
+ShoesFour.addEventListener("click", openProductDetailAsideFour)
+productDetailCloseBotom.addEventListener('click', closeProductDetailAside);
+productDetailCloseBotomtwo.addEventListener('click', closeProductDetailAsidetwo);
+productDetailCloseBotomthree.addEventListener('click', closeProductDetailAsidethree);
+productDetailCloseBotomFour.addEventListener("click", closeProductDetailAsideFour)
 
 function toggleDesktopMenu() {
   let isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 
-  if (!isAsideClosed){
+  if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive')
   }
- desktopMenu.classList.toggle('inactive');
+  desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMobileMenu () {
-let isAsideClosed = shoppingCartContainer.classList.contains('inactive')
+function toggleMobileMenu() {
+  let isAsideClosed = shoppingCartContainer.classList.contains('inactive')
 
-if (!isAsideClosed){
-  shoppingCartContainer.classList.add('inactive')
-}
-closeProductDetailAside();
-
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive')
+  }
+  closeProductDetailAside();
   mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside () {
-let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-let isdesktopMenuClosed = desktopMenu.classList.contains('inactive');
-let isproductDetailCloseBotom = productDetailContainer.classList.contains('inactive');
-if (!isMobileMenuClosed){
-  mobileMenu.classList.add('inactive')
-}
+function toggleCarritoAside() {
+  let isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  let isdesktopMenuClosed = desktopMenu.classList.contains('inactive');
+  let isproductDetailCloseBotom = productDetailContainer.classList.contains('inactive');
 
-if (!isdesktopMenuClosed){
-  desktopMenu.classList.add('inactive')
-}
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive')
+  }
 
-if(!isproductDetailCloseBotom){
-  productDetailContainer.classList.add('inactive')
-}
+  if (!isdesktopMenuClosed) {
+    desktopMenu.classList.add('inactive')
+  }
 
- shoppingCartContainer.classList.toggle('inactive');
-}
+  if (!isproductDetailCloseBotom) {
+    productDetailContainer.classList.add('inactive')
+  }
 
-function openProductDetailAside () {
+  shoppingCartContainer.classList.toggle('inactive');
+}
+//Primer Aside
+function openProductDetailAsideone() {
   shoppingCartContainer.classList.add('inactive');
- productDetailContainer.classList.remove('inactive') ;
+  productDetailContainertwo.classList.add('inactive');
+  productDetailContainerthree.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+  productDetailContainerfour.classList.add('inactive');
+
 }
 
-function closeProductDetailAside () {
-productDetailContainer.classList.add('inactive') ;
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
+}
+//Segundo Aside
+function openProductDetailAsidetwo() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
+  productDetailContainertwo.classList.remove('inactive');
+  productDetailContainerthree.classList.add('inactive');
+  productDetailContainerfour.classList.add('inactive');
 }
 
-const productList = [];
-productList.push({
-  name:'Adidas Lite Racer',
-  price :160,
-  img :'./images/Lite Racer 2.0.jpeg',
-});
-productList.push({
-  name:'Adidas X Speedflow.4 Fxg',
-  price :200,
-  img :'./images/X speedflow.4 .jpeg',
-});
-productList.push({
-  name:'Reebok Runner',
-  price :180,
-  img :'./images/ReebokRunner.jpeg',
-});
+function closeProductDetailAsidetwo() {
+  productDetailContainertwo.classList.add('inactive');
+}
+//Tercer Aside
+function openProductDetailAsidethree() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
+  productDetailContainertwo.classList.add('inactive');
+  productDetailContainerthree.classList.remove('inactive');
+  productDetailContainerfour.classList.add('inactive');
+}
 
-  for (product of productList) {
-    let productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+function closeProductDetailAsidethree() {
+  productDetailContainerthree.classList.add('inactive');
+}
 
-    let productImg = document.createElement('img');
-    productImg.setAttribute('src', product.img);
-    productImg.addEventListener('click' , openProductDetailAside )
-
-    let productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-
-    let productInfoDiv = document.createElement('div');
-
-    let productPrice = document.createElement('p');
-    productPrice.innerText = 'S/' + product.price;
-    let productName = document.createElement('p');
-    productName.innerText = product.name;
-
-    let productInfoFigure = document.createElement('figure');
-    let productImgCart = document.createElement('img');
-    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
-
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-    
-    productInfoFigure.appendChild(productImgCart);
-    
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfoFigure);
-
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
-
-    cardsContainer.appendChild(productCard);
-
-  } 
+//Cuarto Aside
+function openProductDetailAsideFour() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.add('inactive');
+  productDetailContainertwo.classList.add('inactive');
+  productDetailContainerthree.classList.add('inactive');
+  productDetailContainerfour.classList.remove('inactive');
+}
+function closeProductDetailAsideFour() {
+  productDetailContainerfour.classList.add('inactive');
+}
